8fbf54dd9e5c53f087d2a9b05e864befef09265e
Jason <ureadmyemailname@gmail.com>
2016-02-29 08:47:13 +1030

    Update extension.js

--- extension.js
+++ extension.js
@@ -100,7 +100,7 @@ type: 'exact',
 functionality: function (chat, cmd) {
 if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
 if (!bot.commands.executable(this.rank, chat)) return void (0);
-else {API.sendChat("/me Remember to click the star in the top bar to favorite this room!!");}}};
+else {API.sendChat("/me Remember to click the star in the top bar to favorite this room!! http://i.imgur.com/chNflCb.png");}}};
 
 bot.commands.doarefreshCommand = {
 command: 'ref',
